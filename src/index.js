99cf530fad58b3c3c6b2a57d3af6f3f8ca833db2
İsmail Dikici <ismail.dikici@kroppa.com>
2022-01-06 10:59:52 +0300

    langind page & trainer video added

--- src/index.js
+++ src/index.js
@@ -4,5 +4,24 @@ let startExperience = () => {
   window.location.href = "/deneyim.html"
 }
 
+let toggleModal = () => {
+  let video = document.querySelector("video");
+  let modal = document.querySelector(".modal-wrapper");
+  modal.classList.toggle("hide");
+
+  if(video.paused){
+    video.play();
+  }else{
+    video.pause()
+  }
+}
+
 // let button = document.querySelector("#start")
 // button.onclick = startExperience
+
+
+let closeButton = document.querySelector(".close");
+closeButton.onclick = toggleModal;
+
+let showVideo = document.querySelector(".show-video");
+showVideo.onclick = toggleModal;
